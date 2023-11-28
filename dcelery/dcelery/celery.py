fd51719415eab2b3c2a537f1d10fdc22e41892ed
Waheed Khaled <waheedkhaled95@gmail.com>
2023-11-28 22:16:27 +0200

    Test

--- dcelery/dcelery/celery.py
+++ dcelery/dcelery/celery.py
@@ -1,13 +1,25 @@
 import os
 from celery import Celery
+from celery.schedules import crontab
+
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dcelery.settings")
 
 app: Celery = Celery("dcelery")
 app.config_from_object("django.conf:settings", namespace="CELERY")
 
-@app.task
-def add_numbers():
-    return
 
 app.autodiscover_tasks()
+
+app.conf.beat_schedule = {
+    'task-one-every-minute': {
+        'task': 'tasks.task_one',
+        'schedule': crontab(minute='*'),  # Run task_one every minute
+    },
+    'task-two-every-hour': {
+        'task': 'tasks.task_two',
+        'schedule': crontab(minute=0, hour='*'),  # Run task_two every hour
+    },
+    # Add more tasks and their schedules using crontab as needed
+}
+
--- dcelery/newapp/tasks.py
+++ dcelery/newapp/tasks.py
@@ -6,39 +6,14 @@ def sharedtask():
     return
 
 
-class PeriodicTaskController:
-    def __init__(self):
-        self.running = False
-        self.task_instance = None
+@shared_task
+def task_one():
+    # Task one logic
+    print("Task One executed")
 
-    def start_task(self):
-        if not self.running:
-            self.running = True
-            self.task_instance = periodic_task.apply_async()
-            return "Periodic task started."
-        return "Periodic task is already running."
-
-    def stop_task(self):
-        if self.running:
-            self.running = False
-            if self.task_instance:
-                self.task_instance.revoke()
-                self.task_instance = None
-            return "Periodic task stopped."
-        return "No periodic task is running."
-
-controller = PeriodicTaskController()
 
 @shared_task
-def periodic_task():
-    while controller.running:
-        print("Working")
-        time.sleep(20)
+def task_two():
+    # Task two logic
+    print("Task Two executed")
 
-@shared_task
-def start_periodic_task():
-    return controller.start_task()
-
-@shared_task
-def stop_periodic_task():
-    return controller.stop_task()
